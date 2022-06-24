66cd2b6aef758028061b5ea08764be96836a5818
Jefferson Khy <84691651+Jefferson-Khy@users.noreply.github.com>
2022-06-23 19:16:35 -0700

    Update WeatherService.js

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getWeather = () => {
   const config = {
     method: "GET",
-    url: "/data/2.5/onecall?lat=33&lon=-117&appid=0d31656252fe0e66c130129db5d55817&units=imperial",
+    url: "/data/2.5/onecall?lat=33&lon=-117&appid=[insert api key here]&units=imperial",
     withCredentials: true,
     crossdomain: true,
     headers: { "Content-Type": "application/json" },
